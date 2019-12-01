a52740609020b56e85e1b853ec38c00d2024c479
Takeshi Sugiyama <58148757+taksug229@users.noreply.github.com>
2019-12-01 11:25:00 -0800

    analysis

---
+++ Working Data NLP.py
@@ -0,0 +1,78 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re 
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):  
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# TP = # True Positives, TN = # True Negatives, FP = # False Positives, FN = # False Negatives):
+# Accuracy = (TP + TN) / (TP + TN + FP + FN)
+# Precision = TP / (TP + FP)
+# Recall = TP / (TP + FN)
+# F1 Score = 2 * Precision * Recall / (Precision + Recall)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Fitting Decision Tree Classification to the Training set
+# from sklearn.tree import DecisionTreeClassifier
+# classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+# classifier.fit(X_train, y_train)
+
+# Fitting Logistic Regression to the Training set
+# from sklearn.linear_model import LogisticRegression
+# classifier = LogisticRegression(random_state = 0)
+# classifier.fit(X_train, y_train)
+
+# Fitting Random Forest Classification to the Training set
+# from sklearn.ensemble import RandomForestClassifier
+# classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+# classifier.fit(X_train, y_train)
+
+# Fitting SVM to the Training set
+# from sklearn.svm import SVC
+# classifier = SVC(kernel = 'linear', random_state = 0)
+# classifier.fit(X_train, y_train)
+
+# Fitting K-NN to the Training set
+# from sklearn.neighbors import KNeighborsClassifier
+# classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+# classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
